e2a1035155429c41ee0b846c967f66e6d8d49b7f
Zai619 <tracycarriemomo@gmail.com>
2026-04-05 15:05:07 +0800

    加入專案檔案。

---
+++ mortgageCalculator.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace s1131426房貸計算器
+{
+    partial class mortgageCalculator
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTotalPrice = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtSelfFund = new System.Windows.Forms.TextBox();
+            this.txtTotalLoan = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(36, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "房屋總價";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(36, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "自備款比例";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(36, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 33);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "貸款年限";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(36, 326);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 33);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "寬限期";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(36, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(151, 33);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "貸款利率";
+            // 
+            // txtTotalPrice
+            // 
+            this.txtTotalPrice.Location = new System.Drawing.Point(260, 38);
+            this.txtTotalPrice.Name = "txtTotalPrice";
+            this.txtTotalPrice.Size = new System.Drawing.Size(242, 25);
+            this.txtTotalPrice.TabIndex = 5;
+            this.txtTotalPrice.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(260, 193);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(242, 25);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(260, 280);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(242, 25);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(260, 337);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(242, 25);
+            this.textBox4.TabIndex = 8;
+            // 
+            // txtSelfFund
+            // 
+            this.txtSelfFund.Location = new System.Drawing.Point(260, 122);
+            this.txtSelfFund.Name = "txtSelfFund";
+            this.txtSelfFund.Size = new System.Drawing.Size(242, 25);
+            this.txtSelfFund.TabIndex = 9;
+            // 
+            // txtTotalLoan
+            // 
+            this.txtTotalLoan.Location = new System.Drawing.Point(980, 42);
+            this.txtTotalLoan.Name = "txtTotalLoan";
+            this.txtTotalLoan.Size = new System.Drawing.Size(242, 25);
+            this.txtTotalLoan.TabIndex = 13;
+            this.txtTotalLoan.TextChanged += new System.EventHandler(this.txtTotalLoan_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(756, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(185, 33);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "貸款總金額";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(990, 122);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(242, 25);
+            this.textBox7.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(766, 122);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(389, 33);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "每月應繳金額 (本 + 息)";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(980, 197);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(242, 25);
+            this.textBox8.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label8.Location = new System.Drawing.Point(756, 193);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(151, 33);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "首期利息";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(990, 273);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(242, 25);
+            this.textBox9.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label9.Location = new System.Drawing.Point(766, 269);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(151, 33);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "首期本金";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(980, 353);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(242, 25);
+            this.textBox10.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label10.Location = new System.Drawing.Point(756, 349);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(185, 33);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "總利息支出";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(980, 412);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(242, 25);
+            this.textBox11.TabIndex = 23;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label11.Location = new System.Drawing.Point(756, 408);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(185, 33);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "總還款金額";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(578, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 115);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "計算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mortgageCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1244, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtTotalLoan);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtSelfFund);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtTotalPrice);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "mortgageCalculator";
+            this.Text = "房貸計算器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTotalPrice;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtSelfFund;
+        private System.Windows.Forms.TextBox txtTotalLoan;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ mortgageCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace s1131426房貸計算器
+{
+    public partial class mortgageCalculator : Form
+    {
+        public mortgageCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTotalLoan_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
