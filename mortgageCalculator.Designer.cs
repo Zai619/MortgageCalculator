47ae42125ab3a656e847f982cda66c99abc36940
Zai619 <tracycarriemomo@gmail.com>
2026-04-07 19:27:34 +0800

    修改排版

--- mortgageCalculator.Designer.cs
+++ mortgageCalculator.Designer.cs
@@ -41,7 +41,6 @@
             this.txtTotalLoan = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.txtInGrace = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
             this.txtFirstInterest = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
             this.txtFirstPrincipal = new System.Windows.Forms.TextBox();
@@ -54,33 +53,38 @@
             this.txtOutGrace = new System.Windows.Forms.TextBox();
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(36, 34);
+            this.label1.Location = new System.Drawing.Point(36, 48);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(151, 33);
             this.label1.TabIndex = 0;
             this.label1.Text = "房屋總價";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label2.Location = new System.Drawing.Point(36, 111);
+            this.label2.Location = new System.Drawing.Point(36, 122);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(185, 33);
             this.label2.TabIndex = 1;
             this.label2.Text = "自備款比例";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label3.Location = new System.Drawing.Point(36, 252);
+            this.label3.Location = new System.Drawing.Point(36, 279);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(151, 33);
             this.label3.TabIndex = 2;
@@ -90,65 +94,73 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label4.Location = new System.Drawing.Point(36, 326);
+            this.label4.Location = new System.Drawing.Point(36, 347);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(117, 33);
             this.label4.TabIndex = 3;
             this.label4.Text = "寬限期";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label5.Location = new System.Drawing.Point(36, 182);
+            this.label5.Location = new System.Drawing.Point(36, 199);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(151, 33);
             this.label5.TabIndex = 4;
             this.label5.Text = "貸款利率";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // txtTotalPrice
             // 
+            this.txtTotalPrice.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.txtTotalPrice.Location = new System.Drawing.Point(260, 38);
             this.txtTotalPrice.Name = "txtTotalPrice";
-            this.txtTotalPrice.Size = new System.Drawing.Size(242, 25);
+            this.txtTotalPrice.Size = new System.Drawing.Size(242, 51);
             this.txtTotalPrice.TabIndex = 5;
             this.txtTotalPrice.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // txtLoanRate
             // 
+            this.txtLoanRate.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.txtLoanRate.Location = new System.Drawing.Point(260, 193);
             this.txtLoanRate.Name = "txtLoanRate";
-            this.txtLoanRate.Size = new System.Drawing.Size(242, 25);
+            this.txtLoanRate.Size = new System.Drawing.Size(242, 51);
             this.txtLoanRate.TabIndex = 6;
             // 
             // txtLoanLimit
             // 
-            this.txtLoanLimit.Location = new System.Drawing.Point(260, 280);
+            this.txtLoanLimit.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtLoanLimit.Location = new System.Drawing.Point(260, 269);
             this.txtLoanLimit.Name = "txtLoanLimit";
-            this.txtLoanLimit.Size = new System.Drawing.Size(242, 25);
+            this.txtLoanLimit.Size = new System.Drawing.Size(242, 51);
             this.txtLoanLimit.TabIndex = 7;
             this.txtLoanLimit.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // txtGracePeriod
             // 
+            this.txtGracePeriod.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.txtGracePeriod.Location = new System.Drawing.Point(260, 337);
             this.txtGracePeriod.Name = "txtGracePeriod";
-            this.txtGracePeriod.Size = new System.Drawing.Size(242, 25);
+            this.txtGracePeriod.Size = new System.Drawing.Size(242, 51);
             this.txtGracePeriod.TabIndex = 8;
             this.txtGracePeriod.TextChanged += new System.EventHandler(this.txtGracePeriod_TextChanged);
             // 
             // txtSelfFund
             // 
-            this.txtSelfFund.Location = new System.Drawing.Point(260, 122);
+            this.txtSelfFund.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtSelfFund.Location = new System.Drawing.Point(260, 112);
             this.txtSelfFund.Name = "txtSelfFund";
-            this.txtSelfFund.Size = new System.Drawing.Size(242, 25);
+            this.txtSelfFund.Size = new System.Drawing.Size(242, 51);
             this.txtSelfFund.TabIndex = 9;
             // 
             // txtTotalLoan
             // 
-            this.txtTotalLoan.Location = new System.Drawing.Point(980, 42);
+            this.txtTotalLoan.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtTotalLoan.Location = new System.Drawing.Point(980, 38);
             this.txtTotalLoan.Name = "txtTotalLoan";
-            this.txtTotalLoan.Size = new System.Drawing.Size(242, 25);
+            this.txtTotalLoan.Size = new System.Drawing.Size(242, 51);
             this.txtTotalLoan.TabIndex = 13;
             this.txtTotalLoan.TextChanged += new System.EventHandler(this.txtTotalLoan_TextChanged);
             // 
@@ -156,60 +168,54 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label6.Location = new System.Drawing.Point(756, 38);
+            this.label6.Location = new System.Drawing.Point(756, 48);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(185, 33);
             this.label6.TabIndex = 12;
             this.label6.Text = "貸款總金額";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // txtInGrace
             // 
-            this.txtInGrace.Location = new System.Drawing.Point(990, 193);
+            this.txtInGrace.Location = new System.Drawing.Point(232, 58);
             this.txtInGrace.Name = "txtInGrace";
-            this.txtInGrace.Size = new System.Drawing.Size(242, 25);
+            this.txtInGrace.Size = new System.Drawing.Size(242, 47);
             this.txtInGrace.TabIndex = 15;
             this.txtInGrace.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
             // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label7.Location = new System.Drawing.Point(766, 122);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(389, 33);
-            this.label7.TabIndex = 14;
-            this.label7.Text = "每月應繳金額 (本 + 息)";
-            this.label7.Click += new System.EventHandler(this.label7_Click);
-            // 
             // txtFirstInterest
             // 
-            this.txtFirstInterest.Location = new System.Drawing.Point(980, 373);
+            this.txtFirstInterest.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtFirstInterest.Location = new System.Drawing.Point(980, 112);
             this.txtFirstInterest.Name = "txtFirstInterest";
-            this.txtFirstInterest.Size = new System.Drawing.Size(242, 25);
+            this.txtFirstInterest.Size = new System.Drawing.Size(242, 51);
             this.txtFirstInterest.TabIndex = 17;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label8.Location = new System.Drawing.Point(756, 369);
+            this.label8.Location = new System.Drawing.Point(756, 122);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(151, 33);
             this.label8.TabIndex = 16;
             this.label8.Text = "首期利息";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
             // 
             // txtFirstPrincipal
             // 
-            this.txtFirstPrincipal.Location = new System.Drawing.Point(990, 449);
+            this.txtFirstPrincipal.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtFirstPrincipal.Location = new System.Drawing.Point(980, 193);
             this.txtFirstPrincipal.Name = "txtFirstPrincipal";
-            this.txtFirstPrincipal.Size = new System.Drawing.Size(242, 25);
+            this.txtFirstPrincipal.Size = new System.Drawing.Size(242, 51);
             this.txtFirstPrincipal.TabIndex = 19;
+            this.txtFirstPrincipal.TextChanged += new System.EventHandler(this.txtFirstPrincipal_TextChanged);
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label9.Location = new System.Drawing.Point(766, 445);
+            this.label9.Location = new System.Drawing.Point(756, 203);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(151, 33);
             this.label9.TabIndex = 18;
@@ -217,33 +223,37 @@
             // 
             // txtTotalInterest
             // 
-            this.txtTotalInterest.Location = new System.Drawing.Point(980, 529);
+            this.txtTotalInterest.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtTotalInterest.Location = new System.Drawing.Point(980, 269);
             this.txtTotalInterest.Name = "txtTotalInterest";
-            this.txtTotalInterest.Size = new System.Drawing.Size(242, 25);
+            this.txtTotalInterest.Size = new System.Drawing.Size(242, 51);
             this.txtTotalInterest.TabIndex = 21;
             // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label10.Location = new System.Drawing.Point(756, 525);
+            this.label10.Location = new System.Drawing.Point(756, 279);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(185, 33);
             this.label10.TabIndex = 20;
             this.label10.Text = "總利息支出";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
             // 
             // txtTotalPay
             // 
-            this.txtTotalPay.Location = new System.Drawing.Point(980, 588);
+            this.txtTotalPay.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtTotalPay.Location = new System.Drawing.Point(980, 337);
             this.txtTotalPay.Name = "txtTotalPay";
-            this.txtTotalPay.Size = new System.Drawing.Size(242, 25);
+            this.txtTotalPay.Size = new System.Drawing.Size(242, 51);
             this.txtTotalPay.TabIndex = 23;
+            this.txtTotalPay.TextChanged += new System.EventHandler(this.txtTotalPay_TextChanged);
             // 
             // label11
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label11.Location = new System.Drawing.Point(756, 584);
+            this.label11.Location = new System.Drawing.Point(756, 347);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(185, 33);
             this.label11.TabIndex = 22;
@@ -262,9 +272,9 @@
             // 
             // txtOutGrace
             // 
-            this.txtOutGrace.Location = new System.Drawing.Point(990, 280);
+            this.txtOutGrace.Location = new System.Drawing.Point(232, 122);
             this.txtOutGrace.Name = "txtOutGrace";
-            this.txtOutGrace.Size = new System.Drawing.Size(242, 25);
+            this.txtOutGrace.Size = new System.Drawing.Size(242, 47);
             this.txtOutGrace.TabIndex = 25;
             this.txtOutGrace.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
             // 
@@ -272,7 +282,7 @@
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label12.Location = new System.Drawing.Point(766, 193);
+            this.label12.Location = new System.Drawing.Point(28, 61);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(151, 33);
             this.label12.TabIndex = 26;
@@ -283,20 +293,32 @@
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label13.Location = new System.Drawing.Point(779, 280);
+            this.label13.Location = new System.Drawing.Point(28, 125);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(151, 33);
             this.label13.TabIndex = 27;
             this.label13.Text = "寬限期後";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.txtInGrace);
+            this.groupBox1.Controls.Add(this.txtOutGrace);
+            this.groupBox1.Font = new System.Drawing.Font("標楷體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox1.Location = new System.Drawing.Point(762, 440);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(524, 211);
+            this.groupBox1.TabIndex = 28;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "每月應繳金額 (本 + 息)";
             // 
             // mortgageCalculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1311, 675);
-            this.Controls.Add(this.label13);
-            this.Controls.Add(this.label12);
-            this.Controls.Add(this.txtOutGrace);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnRun);
             this.Controls.Add(this.txtTotalPay);
             this.Controls.Add(this.label11);
@@ -306,8 +328,6 @@
             this.Controls.Add(this.label9);
             this.Controls.Add(this.txtFirstInterest);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.txtInGrace);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.txtTotalLoan);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.txtSelfFund);
@@ -324,6 +344,8 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "房貸計算器";
             this.Click += new System.EventHandler(this.mortgageCalculator_Click);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -344,7 +366,6 @@
         private System.Windows.Forms.TextBox txtTotalLoan;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox txtInGrace;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox txtFirstInterest;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox txtFirstPrincipal;
@@ -357,6 +378,8 @@
         private System.Windows.Forms.TextBox txtOutGrace;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- mortgageCalculator.cs
+++ mortgageCalculator.cs
@@ -110,5 +110,50 @@ namespace s1131426房貸計算器
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFirstPrincipal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTotalPay_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
